7aa5beb2309ff2169ecb5804ad8ebd175c98bbba
kola2451 <kolyanemovdev@gmail.com>
2024-08-05 00:03:34 +0300

    Добавил http клиент

--- update-client.go
+++ update-client.go
@@ -6,12 +6,12 @@ import (
 	"github.com/kola24511/stalker-launcher/internal/utils/hash"
 )
 
-func UpdateClient() {
+func UpdateClient() string {
 	directory := "./client"
 	fileHash, err := hash.HashDirectory(directory)
 	if err != nil {
 		fmt.Printf("Ошибка: %v\n", err)
-		return
+		return ""
 	}
-	fmt.Printf("Хэш папки: %s\n", fileHash)
+	return fileHash
 }
