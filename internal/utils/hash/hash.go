1a6a7034758d46b9b14e161bf64777d0ec50420c
kola2451 <kolyanemovdev@gmail.com>
2024-08-29 09:42:41 +0300

    Обновил логику разадчи файлов

--- internal/utils/hash/hash.go
+++ internal/utils/hash/hash.go
@@ -1,30 +1,72 @@
 package hash
 
 import (
-	"crypto/sha256"
+	"crypto/md5"
 	"encoding/hex"
 	"io"
 	"os"
 	"path/filepath"
-	"sort"
+
+	"github.com/kola24511/stalker-launcher/internal/utils/logger"
 )
 
-func HashFile(filepath string) (string, error) {
-	file, err := os.Open(filepath)
+// FileHash хранит информацию о пути к файлу и его хэше
+type FileHash struct {
+	Path string `json:"path"`
+	Hash string `json:"hash"`
+}
+
+// HashFile возвращает MD5-хэш содержимого файла
+func HashFile(filePath string) (string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
+		logger.HandleError(err, "Opening file for hashing")
 		return "", err
 	}
 	defer file.Close()
 
-	hasher := sha256.New()
+	hasher := md5.New()
 	if _, err := io.Copy(hasher, file); err != nil {
+		logger.HandleError(err, "Hashing file content")
 		return "", err
 	}
 
 	return hex.EncodeToString(hasher.Sum(nil)), nil
 }
 
+// GetFilesHashes возвращает хэши всех файлов в указанной директории
+func GetFilesHashes(dir string) ([]FileHash, error) {
+	var files []FileHash
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			logger.HandleError(err, "Walking through directory")
+			return err
+		}
+		if !info.IsDir() {
+			hash, err := HashFile(path)
+			if err != nil {
+				logger.HandleError(err, "Hashing file")
+				return err
+			}
+			relativePath, err := filepath.Rel(dir, path)
+			if err != nil {
+				logger.HandleError(err, "Calculating relative path")
+				return err
+			}
+			files = append(files, FileHash{Path: relativePath, Hash: hash})
+		}
+		return nil
+	})
+	if err != nil {
+		logger.HandleError(err, "Walking through directory")
+		return nil, err
+	}
+	return files, nil
+}
+
 // hashDirectory рекурсивно вычисляет SHA256 хэш директории.
+/*
 func HashDirectory(directory string) (string, error) {
 	var filePaths []string
 
@@ -61,4 +103,5 @@ func HashDirectory(directory string) (string, error) {
 	}
 
 	return hex.EncodeToString(hasher.Sum(nil)), nil
-}
+}8
+*/
--- update-client.go
+++ update-client.go
@@ -1,17 +1,13 @@
 package stalkerlauncher
 
-import (
-	"fmt"
-
-	"github.com/kola24511/stalker-launcher/internal/utils/hash"
-)
-
+/*
 func UpdateClient() string {
 	directory := "./client"
-	fileHash, err := hash.HashDirectory(directory)
+	fileHash, err := hash.GetFilesHashes(directory)
 	if err != nil {
 		fmt.Printf("Ошибка: %v\n", err)
 		return ""
 	}
 	return fileHash
 }
+*/
