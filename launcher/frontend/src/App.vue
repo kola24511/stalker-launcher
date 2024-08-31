988b5a465d1565a880b3df5a37536060383c0b93
kola2451 <kolyanemovdev@gmail.com>
2024-08-31 10:50:48 +0300

    Изменил дизайн

--- launcher/frontend/src/App.vue
+++ launcher/frontend/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
+import background from "@/assets/img/background.jpg";
 
 const { t, availableLocales: languages, locale } = useI18n();
 
@@ -17,31 +18,37 @@ document.body.addEventListener("click", function (event) {
 </script>
 
 <template>
-  <!-- Header -->
-  <div class="w-full h-16 bg-stone-900">
-    <div class="flex items-center justify-between h-full px-4 text-white">
-      <div class="nav space-x-6">
-        <router-link to="/">{{ t("nav.home") }}</router-link>
-        <router-link to="/about">{{ t("nav.about") }}</router-link>
-      </div>
-      <!-- Menu -->
-    <div class="menu">
-      <div class="language flex space-x-4 hover:cursor-pointer">
-        <div
-          v-for="item in languages"
-          :key="item"
-          :class="{ active: item === locale }"
-          @click="onclickLanguageHandle(item)"
-          class="lang-item"
-        >
-          {{ t("languages." + item) }}
+  <div class="relative">
+    <!-- Картинка на заднем фоне -->
+    <img class="absolute inset-0 w-full h-full object-cover z-[-10]" :src="background" />
+
+    <!-- Header -->
+    <div class="w-full h-16 bg-gray-700 bg-opacity-5 backdrop-blur-sm border-b border-gray-700/50">
+      <div class="flex items-center justify-between h-full px-4 text-white">
+        <div class="nav space-x-6">
+          <router-link to="/">{{ t("nav.home") }}</router-link>
+          <router-link to="/about">{{ t("nav.about") }}</router-link>
+        </div>
+        <!-- Menu -->
+        <div class="menu">
+          <div class="language flex space-x-4 hover:cursor-pointer">
+            <div
+              v-for="item in languages"
+              :key="item"
+              :class="{ active: item === locale }"
+              @click="onclickLanguageHandle(item)"
+              class="lang-item"
+            >
+              {{ t("languages." + item) }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
+    <!-- Page -->
+    <div class="view h-[537px] max-h-[537px] relative">
+      <router-view />
     </div>
-  </div>
-  <!-- Page -->
-  <div class="view h-[537px] max-h-[537px]">
-    <router-view />
   </div>
 </template>
